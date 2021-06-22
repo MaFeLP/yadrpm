f710650805ad67d6ffa519ee6dd0c2bdfe78b588
MaFeLP <60669873+MaFeLP@users.noreply.github.com>
2021-06-22 19:50:34 +0200

    ➕ Added building to the make PowerShell Script
    The Script now uses MSBuild.exe (supplied by a flag or the common VS 2019 Installation Directory) to build the final product.

--- powershell-scripts/make.ps1
+++ powershell-scripts/make.ps1
@@ -2,7 +2,8 @@
 
 param (
   [switch]$debug = $null,
-  [switch]$noDebug = $null
+  [switch]$noDebug = $null,
+  [string]$MSBuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
 )
 
 Write-Host "Thank you for using yadrpm by MaFeLP <mafelp@protonmail.ch>!" -ForegroundColor Green
@@ -78,33 +79,44 @@ if ( $DEBUG ) {
 if ( $DEBUG ) {
   Write-Host ":: " -ForegroundColor Cyan -NoNewline
   Write-Host "Building the executable with make..."
-  make
+  & $MSBuild .\yadrpm.sln
   if ( $? ) {
     Write-Host ":: " -ForegroundColor Green -NoNewline
     Write-Host "Successfully built the executable!"
   } else {
     Write-Host ":: " -ForegroundColor Red -NoNewline
     Write-Host "Could not build build the executable!"
-    exit
+    
+    Write-Host ":: " -ForegroundColor Yellow -NoNewline
+    $CLEANUP_INPUT = Read-Host -Prompt ( "Should we clean up nevertheless (default: n)? [y/n]")
+    if ( -Not ($CLEANUP_INPUT -ieq "y") ) {
+      exit
+    }
   }
 } else {
   Write-Host ":: " -ForegroundColor Cyan -NoNewline
   Write-Host "Building the executable with make..."
-  make | Out-Null
+  & $MSBuild .\yadrpm.sln | Out-Null
   if ( $? ) {
     Write-Host ":: " -ForegroundColor Green -NoNewline
     Write-Host "Successfully built the executable!"
   } else {
     Write-Host ":: " -ForegroundColor Red -NoNewline
     Write-Host "Could not build build the executable!"
-    exit
+
+    Write-Host ":: " -ForegroundColor Yellow -NoNewline
+    $CLEANUP_INPUT = Read-Host -Prompt ( "Should we clean up nevertheless (default: n)? [y/n]")
+    if ( -Not ($CLEANUP_INPUT -ieq "y") ) {
+      exit
+    }
   }
 }
 
+#TODO Make the deletion of a file individual in a loop, so no error gets thrown
 # Cleaning the build directory
 Write-Host ":: " -ForegroundColor Cyan -NoNewline
 Write-Host "Cleaning up build directory..."
-Remove-Item -Path ("CMakeCache.txt", "CMakeFiles", "cmake_install.cmake", "Makefile") -Recurse
+Remove-Item -Path ("CMakeCache.txt", "CMakeFiles", "cmake_install.cmake", "out", "x64", "yadrpm.dir", "ALL_BUILD.vcxproj", "ALL_BUILD.vcxproj.filters", "yadrpm.sln", "yadrpm.vcxproj", "yadrpm.vcxproj.filters", "ZERO_CHECK.vcxproj", "ZERO_CHECK.vcxproj.filters") -Recurse
 if ( $? ) {
   Write-Host ":: " -ForegroundColor Green -NoNewline
   Write-Host "Successfully cleaned up build directory!"
